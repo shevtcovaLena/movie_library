6dc7ae95519eac50cabb7f830849138706b609b5
shevtcovaLena <mazepalena@yandex.ru>
2024-06-27 10:46:29 +0300

    eslint-fix

--- client/src/components/Filter/Filter.tsx
+++ client/src/components/Filter/Filter.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-// import api from "../api";
+import { useEffect, useState } from "react";
 import Select from "@mui/material/Select";
 import {
   RadioGroup,
