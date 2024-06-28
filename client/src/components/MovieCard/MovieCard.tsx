2183399909e545d066567ab5ce9a4bf971e7e943
shevtcovaLena <mazepalena@yandex.ru>
2024-06-28 09:56:16 +0300

    lazy-fix

--- client/src/components/MovieCard/MovieCard.tsx
+++ client/src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,6 @@
-// import React from "react";
-import { useContext } from "react";
-import { IconButton } from "@mui/material";
+import { useContext, useState } from "react";
+import { IconButton, Skeleton } from "@mui/material";
+import LazyLoad from "react-lazyload"
 import { IMovie } from "../MovieList/MovieList";
 import styles from "./MovieCard.module.css";
 import { useNavigate } from "react-router-dom";
@@ -12,10 +12,11 @@ interface Props {
   index: number;
 }
 
-export function MovieCard({ movie, index }: Props) {
+export function MovieCard({ movie }: Props) {
   const navigate = useNavigate();
   const { favoriteMovies, toggleFavoriteMovie } = useContext(ContextAll);
   const isFavorite = favoriteMovies.includes(movie.id);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const clickHandler = () => {
     navigate(`/movie/${movie.id}`);
@@ -25,12 +26,15 @@ export function MovieCard({ movie, index }: Props) {
     <div className={styles.container} key={movie.id}>
       <div className={styles.imgbox} onClick={clickHandler}>
         {movie?.poster?.previewUrl || movie?.poster?.url ? (
-          <img
-            loading={index > 10 ? "lazy" : "eager"}
-            decoding={index > 10 ? "async" : "sync"}
-            src={movie.poster.previewUrl || movie.poster.url}
-            alt={movie.name || "No Title"}
-          />
+          <LazyLoad height={300} offset={200} once>
+            {!isLoaded && <Skeleton variant="rectangular" width="100%" height={300} />}
+            <img
+              src={movie.poster.previewUrl || movie.poster.url}
+              alt={movie.name || "No Title"}
+              onLoad={() => setIsLoaded(true)}
+              style={isLoaded ? {} : { display: 'none' }}
+            />
+          </LazyLoad>
         ) : (
           <div className={styles.placeholder}>No Image Available</div>
         )}
@@ -50,12 +54,12 @@ export function MovieCard({ movie, index }: Props) {
       <span>{movie.year}</span>
       {movie.rating.imdb ? (
         <div className={styles.ratingimdb}>
-          <img src="/imdb.svg" />
+          <img src="/imdb.svg" alt="IMDB Rating" />
           <p>{movie.rating.imdb}</p>
         </div>
       ) : (
         <div className={styles.ratingkp}>
-          <img src="/kp.svg" />
+          <img src="/kp.svg" alt="KP Rating" />
           <p>{movie.rating.kp}</p>
         </div>
       )}
